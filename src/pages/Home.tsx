d5e2c466462f4274a19769af5734eff0e9e316c8
Andrew Youngho Chaa <andrew.yh.chaa@gmail.com>
2022-08-21 18:28:46 +0100

    simple grocery list

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,53 +1,31 @@
-import React from 'react'
-import { DotsVerticalIcon } from '@heroicons/react/solid'
+import { useState } from 'react'
 
-const projects = [
-  { name: 'Graph API', initials: 'GA', href: '#', members: 16, bgColor: 'bg-pink-600' },
-  { name: 'Component Design', initials: 'CD', href: '#', members: 12, bgColor: 'bg-purple-600' },
-  { name: 'Templates', initials: 'T', href: '#', members: 16, bgColor: 'bg-yellow-500' },
-  { name: 'React Components', initials: 'RC', href: '#', members: 8, bgColor: 'bg-green-500' },
-]
+export default function Home() {
 
-function classNames(...classes: string[]) {
-  return classes.filter(Boolean).join(' ')
-}
-
-function Home() {
   return (
-    <div>
-    <h2 className="text-gray-500 text-sm font-medium">Pinned Projects</h2>
-    <ul role="list" className="mt-3 grid grid-cols-1 gap-5 sm:gap-6 sm:grid-cols-2 lg:grid-cols-4">
-      {projects.map((project) => (
-        <li key={project.name} className="col-span-1 flex shadow-sm rounded-md">
-          <div
-            className={classNames(
-              project.bgColor,
-              'flex-shrink-0 flex items-center justify-center w-16 text-white text-sm font-medium rounded-l-md'
-            )}
-          >
-            {project.initials}
+    <div className="h-100 w-full flex items-center justify-center bg-teal-lightest font-sans">
+      <div className="bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-lg">
+        <div className="mb-4">
+          <h1 className="text-grey-darkest">Grocery List</h1>
+        </div>
+        <div>
+          <div className="flex mb-4 items-center">
+            <p className="w-full text-grey-darkest">Add another component to Tailwind Components</p>
+            <button className="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded hover:text-white text-green border-green hover:bg-green">Done</button>
+            <button className="flex-no-shrink p-2 ml-2 border-2 rounded text-red border-red hover:text-white hover:bg-red">Remove</button>
           </div>
-          <div className="flex-1 flex items-center justify-between border-t border-r border-b border-gray-200 bg-white rounded-r-md truncate">
-            <div className="flex-1 px-4 py-2 text-sm truncate">
-              <a href={project.href} className="text-gray-900 font-medium hover:text-gray-600">
-                {project.name}
-              </a>
-              <p className="text-gray-500">{project.members} Members</p>
-            </div>
-            <div className="flex-shrink-0 pr-2">
-              <button
-                type="button"
-                className="w-8 h-8 bg-white inline-flex items-center justify-center text-gray-400 rounded-full bg-transparent hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-              >
-                <span className="sr-only">Open options</span>
-                <DotsVerticalIcon className="w-5 h-5" aria-hidden="true" />
-              </button>
-            </div>
+          <div className="flex mb-4 items-center">
+            <p className="w-full line-through text-green">Submit Todo App Component to Tailwind Components</p>
+            <button className="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded hover:text-white text-grey border-grey hover:bg-grey">Not Done</button>
+            <button className="flex-no-shrink p-2 ml-2 border-2 rounded text-red border-red hover:text-white hover:bg-red">Remove</button>
           </div>
-        </li>
-      ))}
-    </ul>
-  </div>  )
+        </div>
+        <div>
+          <div className="flex mt-4">
+            <input type="text" className="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker" placeholder="Add Todo" />
+            <button className="flex-no-shrink p-2 border-2 rounded text-teal border-teal hover:text-white hover:bg-teal">Add</button>
+          </div>
+        </div>
+      </div>
+    </div>)
 }
-
-export default Home
