fd9d2901868959649d4313a9f40534579300c0e3
Andrew Youngho Chaa <andrew.yh.chaa@gmail.com>
2022-08-26 23:16:35 +0100

    add quantity

--- client/src/Home/Add.tsx
+++ client/src/Home/Add.tsx
@@ -4,6 +4,7 @@ import { ADD_GROCERY, GET_GROCERIES } from '../gqls'
 
 export default function Add() {
   const [id, setId] = useState('')
+  const [quantity, setQuantity] = useState(0)
   const [addGrocery] = useMutation(ADD_GROCERY, {
     refetchQueries: [
       {query: GET_GROCERIES}
@@ -13,14 +14,18 @@ export default function Add() {
   return (
     <div>
       <div className="flex mt-4">
-        <input type="text" className="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker" placeholder="Add Todo"
+        <input type="text" className="basis-4/5 shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker" placeholder="Grocery"
           value={id}
           onChange={e => setId(e.target.value)}
+         />
+        <input type="number" className="basis-1/5 shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker"
+          value={quantity}
+          onChange={e => setQuantity(Number(e.target.value))}
          />
         <button className="flex-no-shrink p-2 border-2 rounded text-teal border-teal hover:text-white hover:bg-teal"
           onClick={e => {
             e.preventDefault()
-            addGrocery({variables: {id: id, quantity: 1}})
+            addGrocery({variables: {id: id, quantity: quantity}})
           }}>
           Add
         </button>
